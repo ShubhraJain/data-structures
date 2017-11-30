0e1d9202c348c52be9e0c4b2e15a609e8ae04515
ShubhraJain <00sankalp@gmail.com>
2017-11-29 19:57:39 -0800

    finished queue using fcuntional shared method

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, random.queueMethods);
+  return someInstance;
 };
+var random = {};
 
-var queueMethods = {};
-
+random.queueMethods = {
+  enqueue : function(value) {
+    // Add a string to the back of the queue 
+    var currentSize = this.size();
+    if (currentSize > 0) {
+      for (var i = currentSize; i > -1; i--) {
+        this.storage[i + 1] = this.storage[i];
+      }
+      this.storage[1] = value;
+    } else {
+      this.storage[1] = value;
+    }
+  }, dequeue : function() {
+    // Remove and return the string at the front of the queue
+    var currentSize = this.size();
+    var dequeued = this.storage[currentSize];
+    delete this.storage[currentSize];
+    return '' + dequeued;
+  }, size : function() {
+    // Return the number of items in the queue
+    return Object.keys(this.storage).length;
+  }
+};
 
