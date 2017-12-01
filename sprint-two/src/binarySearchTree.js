69139fcb1feeb32acaf5b389c8273436e43ae72d
ShubhraJain <00sankalp@gmail.com>
2017-11-30 22:09:13 -0800

    completed insert for binary tree using prototypal method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,36 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(binaryTreeMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(target) {
+  var newTree = BinarySearchTree(target);
+  if (this.value < target) {
+    if (this.right === null) {
+      this.right = newTree;
+    } else {
+      (this.right).insert(target);
+    }
+  } else {
+    if (this.left === null) {
+      this.left = newTree;
+    } else {
+      (this.left).insert(target);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(target) {
+  
+};
+
+binaryTreeMethods.depthFirstLog = function(callBack) {
+  
 };
 
 
