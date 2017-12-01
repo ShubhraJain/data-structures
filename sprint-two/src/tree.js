75b11f8593ba5ef08454cca0fa29114efe6e5126
ShubhraJain <00sankalp@gmail.com>
2017-11-30 17:47:57 -0800

    Finished tree implementation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,73 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // base case: no children
+    //determine if search is true
+  // var search = function(obj, searchItem) {
+  //   if (obj.value === searchItem) {
+  //     return true;
+  //   } 
+  // };
+  // //else
+  //   //search || contains on next level
+  // var i = -1;
+  // if (this.children.length === 0) {
+  //   return search(this, target);
+    
+  // } else if (i < this.children.length) {
+  //   i++;
+  //   return search(this, target) || this.contains.call(this.children[i], target);
+  // }
+  var res = false;
+  
+  if (this.value === target) {
+    return true;
+  } 
+  
+  if (this.children) {
+    for (var i = 0; i < this.children.length && res === false; i++) {
+      res = (this.children[i]).contains(target);
+    }
+  }
+  
+  return res;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // var search = function(currNode) {
+  //   // perform check on each of the children in the tree and 
+  //   if (currNode.value === target) {
+  //     return true;
+  //   }
+  // };
+  
+  // if (!this.children.length) {
+  //   search(this);
+  // } else {
+  //   for (var i = 0; i < this.children.length; i++) {
+  //     // if current tree is not equal to target and recurse
+  //     search(this.children[i]);
+  //     this.contains(this.children[i]);
+  //   }
+  // }  
 };
 
 
