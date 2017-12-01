5b0f967d3bb051c8aa656f4a0ee08062fc5e271c
ShubhraJain <00sankalp@gmail.com>
2017-12-01 10:32:08 -0800

    Implemented binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -26,10 +26,39 @@ binaryTreeMethods.insert = function(target) {
 };
 
 binaryTreeMethods.contains = function(target) {
+  // variable doesContain = false
+  var doesContain = false;
+  //  compare target with root node
+  //  if root equals target
+  if (this.value === target) {
+    //  doesContain = true
+    doesContain = true;
+  } else if (this.value < target && this.right !== null) { // if greater 
+    // root becomes first right child
+    // recurse on right child
+    doesContain = this.right.contains(target);
+  } else if (this.value > target && this.left !== null) {
+    // recurse on first left child
+    doesContain = this.left.contains(target);
+  }
   
+  return doesContain;
+
 };
 
 binaryTreeMethods.depthFirstLog = function(callBack) {
+  // call callback on root value
+  callBack(this.value);
+  
+  // call callback on left child
+  if (this.left) {
+    this.left.depthFirstLog(callBack);
+  }
+  
+  // call callback on right child
+  if (this.right) {
+    this.right.depthFirstLog(callBack);
+  }
   
 };
 
